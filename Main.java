34e5d531b493cd84c4d946ea60d5697df16a9b8e
AndreasRWilliams <iicej0ker97@gmail.com>
2024-10-24 14:21:26 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,42 @@
-public class FindFemFejl {
+import java.util.ArrayList;
 
+public class Main {
     public static void main(String[] args) {
-        Cinema biogrande = new Cinema(20, 10);
 
-        // Reserver række 1, sæde 5
-        System.out.println("Række 1, sæde 5 er reserveret: " + biogrande.reserve(1, 5));
+        ArrayList<String> passedCoursesStudent1 = new ArrayList<>();
+        passedCoursesStudent1.add("Java 1.0");
 
-        // Print hele salen pænt
-        System.out.println(biogrande);
+        ArrayList<String> passedCoursesStudent2 = new ArrayList<>();
 
-        // Reserver det sidste sæde i den sidste række
-        int numberOfRows = biogrande.getRows();
-        int numberOfSeats = biogrande.getSeats();
-        biogrande.reserve(numberOfRows, numberOfSeats);
+        ArrayList<String> canTeachTeacher1 = new ArrayList<>();
+        canTeachTeacher1.add("Java 1.0");
 
-        // Afbestil reservation på række 1, sæde 5
-        System.out.println("Række 1, sæde 5 er afbestilt: " + biogrande.cancelReservation(1, 5));
+        ArrayList<String> canTeachTeacher2 = new ArrayList<>();
+        canTeachTeacher2.add("Python 2.0");
+
+
+        Student student1 = new Student("Farid", passedCoursesStudent1);
+        Student student2 = new Student("Jonas", passedCoursesStudent2);
+        Teacher teacher1 = new Teacher("Signe", canTeachTeacher1);
+        Teacher teacher2 = new Teacher("Tine", canTeachTeacher2);
+
+
+        ArrayList<Person> persons = new ArrayList<>();
+        persons.add(student1);
+        persons.add(student2);
+        persons.add(teacher1);
+        persons.add(teacher2);
+
+       
+        for (Person person : persons) {
+            boolean success = person.addCourse("Java 1.0");
+            if (!success) {
+                if (person instanceof Student) {
+                    System.out.println(person.getName() + " har allerede bestået dette kursus.");
+                } else if (person instanceof Teacher) {
+                    System.out.println(person.getName() + " kan ikke undervise i dette fag.");
+                }
+            }
+        }
     }
 }
